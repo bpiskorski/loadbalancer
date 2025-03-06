b9fcce881a108ac8cddcab46f3d515f2ef32352a
bpiskorski <bpiskor97@gmail.com>
2025-03-06 19:10:50 +0100

    changed docker build to image and added check for which server is being used

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import os 
 
 app = Flask(__name__)
-
+server_id = os.environ.get('SERVER_NAME')
 @app.route("/")
-def str():
-    return "Testing load balancer"
+def str():    
+    return "Testing load balancer \nusing server: " + server_id
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
